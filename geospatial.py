ac47654320f9bc3aad6373207e5772ea188fbd5d
Samuel Buit <samuelbuit@mail.weber.edu>
2025-02-04 18:40:03 -0700

    perf: ecm header issues improved for processing all datasets

--- geospatial.py
+++ geospatial.py
@@ -9,6 +9,7 @@ import numpy as np
 import polars as pl
 from scipy.spatial import cKDTree
 from functools import partial
+from tqdm.contrib.concurrent import thread_map
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -19,53 +20,18 @@ class Point:
     lon: np.float32
     deg: bool = True
 
+
 @dataclass
 class Coordinate:
     point: Point
     altitude: np.float32
 
 
-def load_geo_elev_model(json_path: str) -> List[Coordinate]:
-    ft_to_m = 0.3048
-    with open(json_path, 'r') as f:
-        data = json.load(f)
-    profile = []
-    for item in data:
-        profile.append(
-            Coordinate(
-                point=Point(
-                    lat=np.float32(item['lat']),
-                    lon=np.float32(item['lon']),
-                ),
-                altitude=np.float32(item['elev'] * ft_to_m),
-            )
-        )
-
-    return profile
-
-def load_geo_elev_model_v2(json_path: str) -> List[Coordinate]:
-    ft_to_m = 0.3048
-    schema = {'lat': pl.Float32, 'lon': pl.Float32, 'elev': pl.Float32}
-    df = pl.read_json(json_path, schema=schema)
-    df = df.with_columns(pl.col('elev').fill_null(0.0))
-    coords = []
-    for row in df.to_dicts():
-        coords.append(
-            Coordinate(
-                point=Point(
-                    lat=row['lat'],
-                    lon=row['lon'],
-                ),
-                altitude=row['elev'] * ft_to_m,
-            )
-        )
-
-    return coords
-
 class DistanceCalculator(ABC):
     @abstractmethod
     def calculate(self, p1: Point, p2: Point) -> np.float32:
         pass
+
 
 class HaversineDistance(DistanceCalculator):
     def __init__(self, radius: np.float32 = 6367.0):
@@ -78,6 +44,7 @@ class HaversineDistance(DistanceCalculator):
                 lon=np.radians(point.lon),
                 deg=False,
             )
+
         return point
 
     def calculate(self, p1: Point, p2: Point) -> np.float32:
@@ -87,19 +54,18 @@ class HaversineDistance(DistanceCalculator):
         dlon = p2.lon - p1.lon
         a = math.sin(dlat / 2) ** 2 + math.cos(p1.lat) * math.cos(p2.lat) * math.sin(dlon / 2) ** 2
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
         return np.float32(self.radius * c)
+
 
 class Euclidean(DistanceCalculator):
     def calculate(self, p1: Point, p2: Point) -> np.float32:
         return np.sqrt((p2.lat - p1.lat) ** 2 + (p2.lon - p1.lon) ** 2)
 
+
 def _find_closest_point(altitude_data: List[Coordinate],
                         distance_calculator: DistanceCalculator,
                         kdtree, target: Point) -> Coordinate:
-    """
-    Top-level helper function that finds the closest coordinate to the given 
-    target.  This is used in multiprocessing (process_map).
-    """
     if kdtree is not None:
         dist, idx = kdtree.query([float(target.lat), float(target.lon)])
         return altitude_data[idx]
@@ -111,7 +77,9 @@ def _find_closest_point(altitude_data: List[Coordinate],
             if d < min_d:
                 min_d = d
                 closest_coord = coord
+
         return closest_coord
+
 
 class CoordinateMapper:
     def __init__(self, altitude_data: List[Coordinate],
@@ -139,6 +107,7 @@ class CoordinateMapper:
                 if d < min_d:
                     min_d = d
                     closest_coord = coord
+
             return closest_coord
 
     def map_elevations(self, vehicle_data: List[Coordinate]) -> List[np.float32]:
@@ -161,6 +130,45 @@ class CoordinateMapper:
         logging.info(f'Total missing coords: {missing}')
         return [c.altitude if c else np.nan for c in closest_coords]
 
+
+def load_geo_elev_model(json_path: str) -> List[Coordinate]:
+    ft_to_m = 0.3048
+    with open(json_path, 'r') as f:
+        data = json.load(f)
+    profile = []
+    for item in data:
+        profile.append(
+            Coordinate(
+                point=Point(
+                    lat=np.float32(item['lat']),
+                    lon=np.float32(item['lon']),
+                ),
+                altitude=np.float32(item['elev'] * ft_to_m),
+            )
+        )
+
+    return profile
+
+
+def load_geo_elev_model_v2(json_path: str) -> List[Coordinate]:
+    ft_to_m = 0.3048
+    schema = {'lat': pl.Float32, 'lon': pl.Float32, 'elev': pl.Float32}
+    df = pl.read_json(json_path, schema=schema)
+    df = df.with_columns(pl.col('elev').fill_null(0.0))
+    coords = []
+    for row in df.to_dicts():
+        coords.append(
+            Coordinate(
+                point=Point(
+                    lat=row['lat'],
+                    lon=row['lon'],
+                ),
+                altitude=row['elev'] * ft_to_m,
+            )
+        )
+    
+    return coords
+
 def merge_altitudes(original_coords: List[Coordinate], altitudes: List[np.float32]) -> List[Coordinate]:
     updated = []
     for old_coord, new_alt in zip(original_coords, altitudes):
@@ -176,18 +184,21 @@ def merge_altitudes(original_coords: List[Coordinate], altitudes: List[np.float3
 
     return updated
 
+
 def update_vehicle_data_with_elevation(vehicle_data: List[Coordinate],
                                        mapped_elevations: List[np.float32]) -> List[Coordinate]:
     if len(vehicle_data) != len(mapped_elevations):
         logging.error('Mismatch in lengths.')
-        raise ValueError('Mismatch in lengths')
+        # raise ValueError('Mismatch in lengths')
     out = []
     for c, alt in zip(vehicle_data, mapped_elevations):
         out.append(Coordinate(
             point=c.point,
             altitude=alt,
         ))
+
     return out
+
 
 def filter_trackpoints(lat: list[float], lon: list[float], alt: list[float],
                        time_s: list[float], speed_m_s: list[float], dist_thresh: float = 5.0,
@@ -213,21 +224,23 @@ def filter_trackpoints(lat: list[float], lon: list[float], alt: list[float],
         f_alt.append(alt[i])
         f_time.append(time_s[i])
         f_speed.append(speed_m_s[i])
+
     return f_lat, f_lon, f_alt, f_time, f_speed
 
-def calculate_elevation_gain_pipeline(df: pl.DataFrame, window_size=5, threshold=1.0) -> float:
+
+def calculate_elevation_gain_pipeline(df: pl.DataFrame, window_size=50, threshold=1.0) -> float:
     if 'alt_m' not in df.columns:
         return 0.0
-    df_smoothed = df.with_columns(
-        pl.col('alt_m').rolling_mean(window_size).alias('alt_m_smooth')
-    )
+    df_smoothed = df.with_columns(pl.col('alt_m').rolling_mean(window_size).alias('alt_m_smooth'))
     alt_list = df_smoothed['alt_m_smooth'].to_list()
     total_gain = 0.0
     for i in range(1, len(alt_list)):
         delta = alt_list[i] - alt_list[i - 1]
         if delta > threshold:
             total_gain += delta
+
     return total_gain
+
 
 if __name__ == '__main__':
     base_path = './data/vehicle_v2'
