6f867597cd90f428f4268a83b22052225765fff2
Samuel Buit <samuelbuit@mail.weber.edu>
2025-02-25 02:48:28 -0700

    typo

--- nh3/config.py
+++ nh3/config.py
@@ -26,13 +26,13 @@ class ProcessingConfig:
     ELEVATION_MISSING_THRESHOLD: float=10.0  # Meters
     CHUNK_SIZE: int=50
     MAX_WORKERS: int=os.cpu_count()-1
-    GEO_MODEL_PATH: Path=Path('geo/drive_cycle_route.json')  
+    GEO_MODEL_PATH: Path=Path('geo/drive_cycle_route.json')
 
 
 class Config:
-    def __init__(self):        
-        self.BASE_DIR=Path(__file__).parent
-        self.DATA_DIR=self.BASE_DIR / 'data'
+    def __init__(self):
+        self.BASE_DIR=os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../"))
+        self.DATA_DIR=Path(self.BASE_DIR) / 'data'
         self.GEO_MODEL_PATH=self.DATA_DIR / 'geo/drive_cycle_route.json'
         self.OUTPUT_DIR=self.DATA_DIR / 'test_process_output'
         self.EARTH_RADIUS=6_371_000  # Meters
@@ -79,7 +79,7 @@ class Config:
             'canopen_error_reg_0x15': ['CANopen_Error_Reg_0x15()'],
             'ecm_errcode1_0x15': ['ECM_ErrCode1_0x15()'],
             'ecm_auxiliary_0x15': ['ECM_Auxiliary_0x15()'],
-            'ecm_errcode2_0x15': ['ECM_ErrCode2_0x15()'],   
+            'ecm_errcode2_0x15': ['ECM_ErrCode2_0x15()'],
             'nox_node_01': ['NOX(ppm)'],
             'lam': ['lam', 'LAM()'],
             'o2r': ['O2R(%)'],
@@ -103,27 +103,27 @@ class Config:
                         'RPM_$00(rpm)'],
             'obd_vss_kmh_0': ['vss_00', 'VSS_$00(km/h)'],
             'obd_vss_kmh_1': ['VSS_$01(km/h)'],
-            'obd_engine_load_pct': ['load_pct_00', 'LOAD_PCT_$00(%)', 
+            'obd_engine_load_pct': ['load_pct_00', 'LOAD_PCT_$00(%)',
                                     '04h Calculated engine load(%)'],
             'obd_o2s12': ['O2S12_$00(V)'],
             'obd_sup': ['OBDSUP_$00()'],
             'obd_mil_dist': ['mil_dist_00'],
-            'obd_mil_dist_km': ['21h Distance traveled with MIL on(km)', 
+            'obd_mil_dist_km': ['21h Distance traveled with MIL on(km)',
                                 'MIL_DIST_$00(km)'],
             'obd_rpm': ['RPM_$01(rpm)'],
             'obd_clt_dist_km': ['CLR_DIST_$00(km)'],
-            'obd_cat_temp': ['CATEMP11_$00(âˆžC)', 'CATEMP11_$00(Â°C)', 
+            'obd_cat_temp': ['CATEMP11_$00(âˆžC)', 'CATEMP11_$00(Â°C)',
                              'CATEMP11_$00(∞C)', 'CATEMP21_$00(∞C)'],
             'obd_cat_temp_bank_1': ['3Ch Catalyst Temperature: Bank 1'],
             'obd_o2_sensor_bank2': ['13h O2 sensors present (in 2 banks)()'],
             'obd_O2_S12_v': ['O2S12_$00(V)'],
             'obd_maf_gs': ['MAF_$00(g/s)'],
-            'obd_mil_dist_km': ['MIL_DIST_$00(km)', 
+            'obd_mil_dist_km': ['MIL_DIST_$00(km)',
                                 '21h Distance traveled with MIL on(km)'],
             'obd_iat': ['IAT_$00(âˆžC)', 'IAT_$00(∞C)'],
             'obd_cat_rdy': ['CAT_RDY_$00()'],
             'obd_speed_kmh': ['0Dh Vehicle speed(km/h)'],
-            'obd_intake_air_temp': ['0Fh Intake air temperature(âˆžC)', 
+            'obd_intake_air_temp': ['0Fh Intake air temperature(âˆžC)',
                                     '0Fh Intake air temperature(∞C)'],
             'obd_abs_barometric_kpa':['33h Absolute Barometric Pressure(kPa)']
 }
